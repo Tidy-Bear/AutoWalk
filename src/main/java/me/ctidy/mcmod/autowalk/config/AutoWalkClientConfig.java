262988485d4cb258b3fcc7b5b0d6c3a7ce21153c
Tidy-Bear <ChenTidyBear@outlook.com>
2023-08-31 06:03:43 +0800

    Modify: Make config comments i18n. Update: Lang i18n.

--- src/main/java/me/ctidy/mcmod/autowalk/config/AutoWalkClientConfig.java
+++ src/main/java/me/ctidy/mcmod/autowalk/config/AutoWalkClientConfig.java
@@ -1,6 +1,7 @@
 package me.ctidy.mcmod.autowalk.config;
 
 import com.electronwill.nightconfig.core.Config;
+import me.ctidy.mcmod.autowalk.ModEnvConstants;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.ForgeConfigSpec;
@@ -29,17 +30,28 @@ public class AutoWalkClientConfig {
         INSTANCE = specPair.getLeft();
     }
 
+    private static String nameTKey(final String key) {
+        return ModEnvConstants.MOD_ID + ".config." + key;
+    }
+
+    private static String commentTKey(final String key) {
+        return nameTKey(key) + ".comment";
+    }
+
     public AutoWalkClientConfig(ForgeConfigSpec.Builder builder) {
         builder.push("hud");
 
         showHud = builder.comment("Whether show message horizontally centered on the screen when auto walking.")
+                .translation(nameTKey("show_hud"))
                 .define("enable", true);
         hudOffsetY = builder.comment("The vertical offset from the top of the screen.")
+                .translation(nameTKey("hud_offset_y"))
                 .define("offset_y", 10);
 
         builder.pop().push("action");
 
         autoJump = builder.comment("When set to true, player can auto jump when auto walking.\nWhen set to false, it will depend on the vanilla settings.")
+                .translation(nameTKey("auto_jump"))
                 .define("auto_jump", true);
 
         builder.pop();
